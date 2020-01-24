f615aeabff77d2402086b5c917714beeb76020ee
Tarciso Torres <tarcisoftorres@gmail.com>
2020-01-23 22:36:26 -0300

    Trabalhando com multiplos slots

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <template>
-  <div id="app">
+  <div id="app" class="container">
     <h1>Vue js</h1>
     <Post>
-      <h2>Components no Vue</h2>
-      <p>Componentes são uma das peças mais importantes no Vue</p>
+      <h2 slot="cabecalho">Components no Vue</h2>
+      <template slot="conteudo">
+        <p>Componentes são uma das peças mais importantes no Vue</p>
+        <span>...</span>
+      </template>
+      <small slot="rodape">Por Tarciso Torres</small>
     </Post>
   </div>
 </template>
@@ -18,5 +22,12 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+  .container {
+    width: 960px;
+    margin: auto;
+  }
+</style>
 
 
