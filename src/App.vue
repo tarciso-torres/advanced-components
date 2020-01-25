5a91d5f418ffce3210d402425a584e400648ec53
Tarciso Torres <tarcisoftorres@gmail.com>
2020-01-25 14:04:46 -0300

    Atribuiçao via desestruturaçao no slot-escope

--- src/App.vue
+++ src/App.vue
@@ -8,10 +8,10 @@
     <h1>Slots com escopo</h1>
 
     <PostsLista :posts="posts">
-      <template slot-scope="slotProps">
-        <h2>{{ slotProps.meuPost.titulo }}</h2>
-        <p>{{ slotProps.meuPost.conteudo }}</p>
-        <small>{{ slotProps.meuPost.autor }}</small>
+      <template slot-scope="{ meuPost }">
+        <h2>{{ meuPost.titulo }}</h2>
+        <p>{{ meuPost.conteudo }}</p>
+        <small>{{ meuPost.autor }}</small>
       </template>
     </PostsLista>
     
