559799ba6cc1b5a9683858e78cc4cf8cac85dd23
Tarciso Torres <tarcisoftorres@gmail.com>
2020-02-17 13:48:00 -0300

    Entendendo como funcionam os componentes dinamicos

--- src/components/Sobre.vue
+++ src/components/Sobre.vue
@@ -1,3 +1,18 @@
 <template>
-    <h2>Sobre</h2>
+    <div>
+        <h2>Sobre</h2>
+
+        <p v-if="autor">Autor: {{ autor }}</p>
+        <input type="text" v-model="autor" />
+    </div>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            autor: ''
+        }
+    }
+}
+</script>
